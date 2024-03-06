50d072786144cecbfe7ab7e7251fa66622cba895
Jack Roybal <excusemeimjack@gmail.com>
2024-03-06 12:40:21 -0800

    Added Nav and Footer

--- src/slices/Hero/Shapes.jsx
+++ src/slices/Hero/Shapes.jsx
@@ -8,7 +8,7 @@ import { gsap } from "gsap";
 
 export default function Shapes(){
   return (
-    <div className="row-span-1 row-start-1 -mt-9 aspect-square md:col-span-1 md:col-start-2 md:mt-0">
+    <div className="row-span-1 row-start-1 -mt-9 aspect-square md:col-span-1 md:col-start-2 md:mt-0 select-none">
       <Canvas className="z-0" shadows gl = {{antialias:false}} dpr={[1, 1.5]} camera={{postion: [0, 0, 25], fov: 30, next:1, far:40}}>
         <Suspense fallback={null}>
           <Geometries />
