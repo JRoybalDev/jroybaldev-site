af571f899190353a0b090a83ee9e318c8d140ed6
Jack Roybal <excusemeimjack@gmail.com>
2024-03-08 10:43:25 -0800

    Added Tech

--- src/slices/TechList/index.tsx
+++ src/slices/TechList/index.tsx
@@ -1,15 +1,16 @@
-'use client'
+"use client";
 
-import Bounded from "@/components/Bounded";
-import Heading from "@/components/Heading";
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
-import React, { useEffect, useRef } from "react";
+import React, { useLayoutEffect, useRef } from "react";
 import { MdCircle } from "react-icons/md";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-gsap.registerPlugin(ScrollTrigger)
+import Bounded from "@/components/Bounded";
+import Heading from "@/components/Heading";
+
+gsap.registerPlugin(ScrollTrigger);
 
 /**
  * Props for `TechList`.
@@ -20,18 +21,18 @@ export type TechListProps = SliceComponentProps<Content.TechListSlice>;
  * Component for "TechList" Slices.
  */
 const TechList = ({ slice }: TechListProps): JSX.Element => {
-
   const component = useRef(null);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     let ctx = gsap.context(() => {
+      // create as many GSAP animations and/or ScrollTriggers here as you want...
       const tl = gsap.timeline({
         scrollTrigger: {
-          trigger: component.current,
+          pin: true, // pin the trigger element while active
           start: "top bottom",
           end: "bottom top",
-          scrub: 1
-        }
+          scrub: 4,
+        },
       });
 
       tl.fromTo(
@@ -40,27 +41,27 @@ const TechList = ({ slice }: TechListProps): JSX.Element => {
           x: (index) => {
             return index % 2 === 0
               ? gsap.utils.random(600, 400)
-              : gsap.utils.random(-600, -400)
-          }
+              : gsap.utils.random(-600, -400);
+          },
         },
         {
           x: (index) => {
             return index % 2 === 0
               ? gsap.utils.random(-600, -400)
-              : gsap.utils.random(600, 400)
+              : gsap.utils.random(600, 400);
           },
-          ease: "power1.inOut"
-        }
-        )
+          ease: "power1.inOut",
+        },
+      );
     }, component);
-
-    return () => ctx.revert();
+    return () => ctx.revert(); // cleanup!
   }, []);
 
   return (
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="wrapper overflow-hidden"
       ref={component}
     >
       <Bounded as="div">
@@ -68,19 +69,24 @@ const TechList = ({ slice }: TechListProps): JSX.Element => {
           {slice.primary.heading}
         </Heading>
       </Bounded>
-      {slice.items.map(({tech_color, tech_name}, index) => (
+
+      {slice.items.map(({ tech_color, tech_name }, index) => (
         <div
           key={index}
           className="tech-row mb-8 flex items-center justify-center gap-4 text-slate-700"
-          aria-label={tech_name || undefined}
-          >
-
-          {Array.from({length: 15}, (_, index) => (
+          aria-label={tech_name || ""}
+        >
+          {Array.from({ length: 15 }, (_, index) => (
             <React.Fragment key={index}>
               <span
-                className="tech-item text-8xl font-extrabold uppercase tracking-tighter"
-                style={{color: index === 7 && tech_color ? tech_color : "inherit"}}>
-                  {tech_name}
+                className={
+                  "tech-item text-8xl font-extrabold uppercase tracking-tighter"
+                }
+                style={{
+                  color: index === 7 && tech_color ? tech_color : "inherit",
+                }}
+              >
+                {tech_name}
               </span>
               <span className="text-3xl">
                 <MdCircle />
